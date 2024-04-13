6270a6b2646bd1f7beb3d2b91817a07600d96574
Jesse Wei <jesse@cs.unc.edu>
2024-04-13 11:14:19 -0400

    Nits

--- client.py
+++ client.py
@@ -1,20 +1,21 @@
-import socket
+from socket import socket, AF_INET, SOCK_STREAM
+from constants import PORT
 
 # Create a TCP/IP socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = socket(AF_INET, SOCK_STREAM)
 
 # Connect the socket to the server's IP address and port
-server_address = ('localhost', 12345)
+server_address = ("localhost", PORT)
 client_socket.connect(server_address)
 
 try:
     # Send data to the server
-    message = 'Hello, server!'
+    message = "Hello, server!"
     client_socket.sendall(message.encode())
 
     # Receive the server's response
     data = client_socket.recv(1024)
-    print(f'Received: {data.decode()}')
+    print(f"Received: {data.decode()}")
 
 finally:
     # Close the connection
--- server.py
+++ server.py
@@ -4,24 +4,24 @@ from constants import PORT
 # Create TCP/IP socket
 server_socket: socket = socket(AF_INET, SOCK_STREAM)
 
-server_address: tuple[str, int] = ('localhost', PORT)
+server_address: tuple[str, int] = ("localhost", PORT)
 server_socket.bind(server_address)
 
 server_socket.listen(5)
-print('Server is listening...')
+print("Server is listening...")
 
 while True:
     # Wait for a connection
     client_socket, client_address = server_socket.accept()
-    print(f'Connection from {client_address}')
+    print(f"Connection from {client_address}")
 
     # Receive data from the client
     data = client_socket.recv(1024)
     if data:
-        print(f'Received: {data.decode()}')
+        print(f"Received: {data.decode()}")
 
         # Send a response back to the client
-        response = 'Message received'
+        response = "Message received"
         client_socket.sendall(response.encode())
 
     # Close the connection

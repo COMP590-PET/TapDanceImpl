b522f9a623a4d837d395a8132a7801564ea62110
Jesse Wei <jesse@cs.unc.edu>
2024-04-15 16:16:13 -0400

    Site blocking functionality (#1)
    * Add sample client and server (on same IP) using socket library
    
    * Nits
    
    * Update client.py and server.py
    
    * Use mitmproxy for blocking websites
    
    * Delete client.py

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # README
+
+## Install
+
+Install `mitmproxy` with pip and tmux.
+
+## Usage
+
+Open two tmux panes.
+
+In one, run `mitmdump -s ./censor.py`.
+
+In the other, run `curl --proxy http://127.0.0.1:8080 "https://www.google.com"` (will be blocked). Getting any other URL will work fine.
---
+++ censor.py
@@ -0,0 +1,23 @@
+from mitmproxy import http
+import logging
+import re
+
+BLOCKED_URLS = []
+
+class BlockResource:
+    def __init__(self):
+        for re_url in open("urls.txt"):
+            BLOCKED_URLS.append(re.compile(re_url.strip()))
+        logging.info(f"{len(BLOCKED_URLS)} urls read")
+
+
+    def request(self, flow: http.HTTPFlow) -> None:
+        if any(re.search(url, flow.request.url) for url in BLOCKED_URLS):
+            logging.info(f"found match for {flow.request.url}")
+            flow.response = http.Response.make(
+                404, b"You have visited a blocked URL\n", {"Content-Type": "text/plain"}
+            )
+
+addons = [
+    BlockResource()
+]

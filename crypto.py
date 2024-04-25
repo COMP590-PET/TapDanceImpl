a5822ae551ef0fecbaf43e35509927d1c6102ff2
dkim-unc <dkim3@unc.edu>
2024-04-25 17:52:51 -0400

    crypto outline

---
+++ crypto.py
@@ -0,0 +1,24 @@
+import cryptography
+import pymonocypher as cypher
+import os
+
+
+
+def Curve25519():
+
+def TagGen(Key SSkey):
+    tweak = os.urandom(1)
+    cypher.crypto_elligator_rev(SSkey, , tweak)
+
+
+
+def encode(### request, Key Pkey, Tag):       ##attach tag
+    self.TagGen()
+
+def decode(### request):       ##recover tag
+    
+    return tag
+
+def extract(tag):      ##extract key
+    cypher.crypto_elligator_map
+    

8278153af639bf633f6288ab44052b03e43c3a33
Jesse Wei <jesse@cs.unc.edu>
2024-04-25 13:51:21 -0400

    Nitpick client.py and reformat files

--- censor.py
+++ censor.py
@@ -1,19 +1,18 @@
+# Usage: mitmdump -s ./censor.py
+
 from mitmproxy import http
 import logging
 import re
 
-CENSOR_BLOCKED_URLS = []
-ISP_REFRACT_URLS = []
+CENSOR_BLOCKED_URLS: list[re.Pattern] = list(map(re.compile, [r"bing\.com"]))
+ISP_REFRACT_URLS: list[re.Pattern] = list(map(re.compile, [r"youtube\.com"]))
+"""When visiting any of these links, refract to somewhere else"""
 
 
 # From https://dev.to/dandyvica/use-mitmproxy-as-a-personal-firewall-4m6h
 class BlockResource:
     def __init__(self):
-        for re_url in open("censor_blocked_urls.txt"):
-            CENSOR_BLOCKED_URLS.append(re.compile(re_url.strip()))
         logging.info(f"{len(CENSOR_BLOCKED_URLS)} censored urls read")
-        for re_url in open("isp_refract_urls.txt"):
-            ISP_REFRACT_URLS.append(re.compile(re_url.strip()))
         logging.info(f"{len(ISP_REFRACT_URLS)} refract urls read")
 
     def request(self, flow: http.HTTPFlow) -> None:
@@ -28,5 +27,6 @@ class BlockResource:
         if any(re.search(url, flow.request.url) for url in ISP_REFRACT_URLS):
             logging.info(f"isp found match for {flow.request.url}")
             flow.request.host = "www.reddit.com"
+
 
 addons = [BlockResource()]
--- client.py
+++ client.py
@@ -1,9 +1,15 @@
+# Usage: After running censor.py (following the usage instructions there), run python -m client
+
 import requests
 
-proxies = {
-    "http": "http://127.0.0.1:8080",
-}
+PROXIES = {"http": "http://127.0.0.1:8080", "https": "https://127.0.0.1:8080"}
 
-response = requests.get("http://www.google.com", proxies=proxies)
-print(f"Status code: {response.status_code}")
-print(f"Text: {response.text}")
+
+def main() -> None:
+    response = requests.get("http://www.google.com", proxies=PROXIES)
+    print(f"Status code: {response.status_code}")
+    print(f"Text: {response.text}")
+
+
+if __name__ == "__main__":
+    main()

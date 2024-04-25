17f4b9a347697f4a1a363e2a0f7baae767e3d29e
Jesse Wei <jesse@cs.unc.edu>
2024-04-25 14:45:21 -0400

    Add censorship unit test

--- constants.py
+++ constants.py
@@ -1,7 +1,14 @@
 import re
+from pathlib import Path
 
 CENSOR_BLOCKED_URLS: list[re.Pattern] = list(
     map(re.compile, [r"bing\.com", r"duckduckgo\.com"])
 )
 ISP_REFRACT_URLS: list[re.Pattern] = list(map(re.compile, [r"reddit\.com"]))
 """When visiting any of these links, refract to somewhere else"""
+
+PROXIES: dict[str, str] = {
+    "http": "http://127.0.0.1:8080",
+    "https": "https://127.0.0.1:8080",
+}
+MITMPROXY_CERT: Path = Path("resources") / "mitmproxy-ca-cert.pem"

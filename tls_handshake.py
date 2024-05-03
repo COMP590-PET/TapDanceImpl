2a4aab9589e979f1d233af06d7bc82d2fd60efe1
Jesse Wei <jesse@cs.unc.edu>
2024-05-02 23:33:16 -0400

    tls handshake master key that goes through proxy server

--- tls_handshake.py
+++ tls_handshake.py
@@ -1,18 +1,19 @@
 from OpenSSL import SSL, crypto
-from OpenSSL.SSL import Context, TLS_METHOD, TLS_CLIENT_METHOD, Connection
+from OpenSSL.SSL import Context, TLS_METHOD, TLS_CLIENT_METHOD, TLSv1_2_METHOD, Connection
 from socket import socket, AF_INET, SOCK_STREAM, gethostbyname, create_connection
 from constants import BADSSL_TLS_1_2_PORT
 
-site: str = "tls-v1-2.badssl.com"
+site: str = f"tls-v1-2.badssl.com:{BADSSL_TLS_1_2_PORT}"
 
-ctx: Context = Context(TLS_CLIENT_METHOD)
+ctx: Context = Context(TLSv1_2_METHOD)
 
-sock: socket = socket(AF_INET, SOCK_STREAM)
-sock.connect((site, BADSSL_TLS_1_2_PORT))
-connection: Connection = Connection(ctx, sock)
+proxy_sock: socket = socket(AF_INET, SOCK_STREAM)
+proxy_sock.connect(("127.0.0.1", 8080))
+connection: Connection = Connection(ctx, proxy_sock)
+connection.set_tlsext_host_name(site.encode())
 connection.set_connect_state()
 connection.do_handshake()
-print(f"Did handshake with {site} over port {BADSSL_TLS_1_2_PORT}")
+print(f"Did handshake with {site} over port {BADSSL_TLS_1_2_PORT} through proxy server {proxy_sock.getpeername()}")
 
 master_key = connection.master_key()
 print(f"TLS handshake master key: {master_key}")
